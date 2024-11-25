1e56dcb7cf66297b1ebe1a8fb410343f6fefec04
DerfM53 <frederic.mieuzet@gmail.com>
2024-11-25 14:05:13 +0100

    add file for task 9 project 0

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList() {
   const entries = [
-    ['Apples', '10'],
-    ['Tomatoes', '10'],
-    ['Pasta', '1'],
-    ['Rice', '1'],
-    ['Banana', '5'],
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
   ];
   const myMap = new Map(entries);
   return myMap;
