34c0ac64e283cf62020b0e89a925da41b9e75698
DerfM53 <frederic.mieuzet@gmail.com>
2024-12-04 16:19:30 +0100

    import typing for List and add Docstrin for function

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,8 +5,9 @@ This module contain a function who take a list of
 the integer and floats and return the sum of the
 numbers of the list
 """
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Returns the sum of the mxd_lst"""
     return sum(mxd_lst)
