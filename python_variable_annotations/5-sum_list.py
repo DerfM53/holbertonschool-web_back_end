d528133876b643ae8627ebdcd3c2eb1056c6bf0c
DerfM53 <frederic.mieuzet@gmail.com>
2024-12-04 16:18:01 +0100

    import typing for List and add Docstrin for function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
 """
 This module contain a function take a list of the
 float and return the sum of the number of the same list.
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
+    """Returns their sum as float"""
     return sum(input_list)
